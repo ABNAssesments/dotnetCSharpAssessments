336331da934f60850bfb81919f0d4e44c1cbb0e3
Filip <phichoo@gmail.com>
2024-10-21 23:03:52 +0200

    7 segments string to number mapping added

---
+++ SevenSegmentReader/SevenSegmentConstants.cs
@@ -0,0 +1,13 @@
+public static class SevenSegmentConstants
+{
+    public const string Zero = "1110111";
+    public const string One = "0010010";
+    public const string Two = "1011101";
+    public const string Three = "1011011";
+    public const string Four = "0111010";
+    public const string Five = "1101011";
+    public const string Six = "1101111";
+    public const string Seven = "1010010";
+    public const string Eight = "1111111";
+    public const string Nine = "1111011";
+}
