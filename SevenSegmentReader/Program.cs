a0124d5fe2c4560d4db66940472cfac050de0311
Filip <phichoo@gmail.com>
2024-10-21 23:11:07 +0200

    added choice loop

--- SevenSegmentReader/Program.cs
+++ SevenSegmentReader/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("1. convert segment data to number");
+            Console.WriteLine("2. convert number to segment data");
+            Console.WriteLine("3. exit");
+
+            Console.Write("Choose an option (1-3): ");
+
+            string? choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    return;
+                default:
+                    Console.WriteLine("Invalid option. Please try with new input.");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+}
